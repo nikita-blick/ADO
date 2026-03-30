aa45902c5481ae5a73cd4b8340d75531d41cd95a
Nikita <Nikita@DESKTOP-13IEN5T>
2026-03-30 19:06:32 +0500

    Directions in progress

---
+++ Academy/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Academy
+{
+	public partial class MainForm : Form
+	{
+		DBtools.Connector connector;
+		public MainForm()
+		{
+
+			InitializeComponent();
+			connector = new DBtools.Connector(ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString);
+			dgvDirections.DataSource = connector.Select("*", "Directions");
+			//toolStripStatusLabel.Text = $"Количество направлений обучения: {dgvDirections.Rows.Count - 1}";
+			//toolStripStatusLabel.Text = $"Количество направлений обучения: {connector.Scalar("SELECT COUNT(*) FROM Directions")}";
+			//tabControl_SelectedIndexChanged(tabControl, null);
+
+		}
+	}
+}
